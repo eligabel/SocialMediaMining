5fd23744c7919cb514eaf318df1458bf64fe83a4
Lizzy Gabel <eligabel@iu.edu>
2023-01-05 19:23:16 -0500

    Code

--- Project2/Code/n-grams.py
+++ Project2/Code/n-grams.py
@@ -1,13 +1,34 @@
-from sklearn import svm
-from sklearn.svm import SVC
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+from nltk.tokenize import word_tokenize, sent_tokenize
+import pandas as pd
+from pandas import Series,DataFrame
+import numpy as np
+import re
 import pandas as pd
 
-dataPath = r'C:\Users\irisv\Documents\SupplementalProject2\Data\mergedAskRed.csv'
-df = pd.read_csv(dataPath)
+dataPath = '~/SocialMediaMining/Project2/Data/mergedMensRightsRed.csv'
+df = pd.read_csv(dataPath, engine='python')
 
+#unigrams
 words = df.comment_body
 
-print(words)
+#bigrams
+bigrams = (pd.Series(nltk.ngrams(words, 3)).value_counts())
+
+#trigrams
+trigrams = (pd.Series(nltk.ngrams(words, 3)).value_counts())
+
+#4-grams
+frGrams = (pd.Series(nltk.ngrams(words, 4)).value_counts())
+
+#new column in df
+df['unigrams'] = words
+df['bigrams'] = bigrams
+df['trigrams'] = trigrams
+df['4-grams'] = frGrams
+
+#Export
+print('exporting POS tagged file')
+df.to_csv('~/SocialMediaMining/Project2/Data/mergedAskRedGrams', index=False)
